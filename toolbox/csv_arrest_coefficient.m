561ad5d4db2512e715278b51467f8c45c5f38629
barclaylee <barclaylee@barclays-mini.ad.bcm.edu>
2017-08-01 18:31:17 -0500

    test

---
+++ toolbox/csv_arrest_coefficient.m
@@ -0,0 +1,34 @@
+%% To modify the layout the measurements made by Imaris
+% This script takes the Imaris 'Speed' export csv file where all the measurement
+% are listed time point after time point and calculates the arrest
+% coefficient based on user-set threshold
+clear all
+close all
+
+%% Loading and various parameters
+
+threshold = 2 / 60; % um/s threshold for arrest coefficient
+[filename, path, ~] = uigetfile('.csv'); %Select Speed file
+
+Extracted_data = parse_csv([path filename]);
+
+%% Separate the main table into individual tables per TruelyUniqueName
+
+[TUN, TUN_idx_last, TUN_idx] = unique(Extracted_data(:,'TrackID'),'stable');
+disp(['Number of trajectories: ', mat2str(height(TUN))]);
+unique_idx = accumarray(TUN_idx(:),(1:length(TUN_idx))',[],@(x) {sort(x)});
+indicator_progress = 0;
+arrest_coeffs = [];
+
+for jj = 1:height(TUN);
+    speeds=Extracted_data{unique_idx{jj},'Speed'};
+    num_arrest = length(find(speeds < threshold));
+    tmp_coeff = num_arrest / length(speeds);
+    arrest_coeffs = [arrest_coeffs; tmp_coeff];
+    
+end
+
+tmp_table = horzcat(table(arrest_coeffs),TUN);
+str_header = min(find(filename == '_'));
+writetable(tmp_table, [path '/' filename(1:str_header) 'Arrest_Coefficient.csv']);
+disp(['Wrote: ' filename(1:str_header) 'Arrest_Coefficient.csv']);
---
+++ toolbox/csv_position_imaris.m
@@ -0,0 +1,41 @@
+%% To modify the layout of the measurements made by Imaris
+% This script takes the Imaris 'Position' export csv file where all the measurement
+% are listed time point after time point and re-shuffle them into collated
+% columns with one track per column
+clear all;
+close all; 
+
+%% Loading and various parameters
+time_scale = 2; % time interval (in min)
+
+[filename, path, ~] = uigetfile('.csv'); %select Position file
+Extracted_data = parse_csv([path filename]);
+%% Separate the main table into individual tables per TruelyUniqueName
+
+% [TUN, TUN_idx_last, TUN_idx] = unique(Extracted_data(:,13),'stable'); % Use this line if you've generated unique TrackIDs above
+[TUN, TUN_idx_last, TUN_idx] = unique(Extracted_data(:,'TrackID'),'stable');
+disp(['Number of trajectories: ', mat2str(height(TUN))]);
+unique_idx = accumarray(TUN_idx(:),(1:length(TUN_idx))',[],@(x) {sort(x)});
+textprogressbar('Progress:   ');
+indicator_progress = 0;
+tracks = {};
+for jj = 1:height(TUN);
+    temporary_table=Extracted_data(unique_idx{jj},:);
+    temporary_track=[temporary_table.Time*time_scale temporary_table.PositionX temporary_table.PositionY];
+    % filename_export = strjoin([pathname '/' temporary_table.TruelyUniqueName(1) '.csv'],''); % Use this line if you've generated unique TrackIDs above
+    tracks = cat(1, tracks, temporary_track);
+    
+    indicator_progress = indicator_progress + (100/height(TUN));
+    textprogressbar(indicator_progress);
+end
+textprogressbar(' Done');
+clear jj indicator_progress TUN* unique_idx Extracted_data pathname
+
+str_header = min(find(filename == '_'));
+save([path filename(1:str_header) 'tracks_for_MSDanalyzer.mat'], 'tracks');
+disp(['Wrote: ' filename(1:str_header) 'tracks_for_MSDanalyzer.mat']);
+
+clearvars -except tracks path filename str_header
+
+%% Calculate constrained/directed/random transient migratory periods
+
